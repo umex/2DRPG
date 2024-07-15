676e39d3d29a8ffb8b6c30d8d6abfc7c3c0bad7b
dobrez <dejan.obrez@gmail.com>
2024-07-15 22:36:57 +0200

    added regions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region Components
     public Animator anim { get; private set; }
+    #endregion
 
+    #region States
     public PlayerStateMachine stateMachine { get; private set; }
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
-
+    #endregion
 
     protected void Awake()
     {
