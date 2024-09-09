a5e8fcc87a7eb17d3526cb5877aea1e4d724e526
dobrez <dejan.obrez@gmail.com>
2024-09-09 20:45:42 +0200

    fixed a bug with attack direction

--- Assets/Scripts/Player/PlayerPrimaryAttackState.cs
+++ Assets/Scripts/Player/PlayerPrimaryAttackState.cs
@@ -16,6 +16,7 @@ public class PlayerPrimaryAttackState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        xInput = 0; // we need this to fix bug on attack direction
 
         if (comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
         {
