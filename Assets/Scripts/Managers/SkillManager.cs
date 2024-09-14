3563254df4c45d1af85cfd8eff1ccb1bf754096c
dobrez <dejan.obrez@gmail.com>
2024-09-14 14:27:27 +0200

    Added SkillManager singleton

---
+++ Assets/Scripts/Managers/SkillManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+    public static SkillManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(instance.gameObject);
+        else
+            instance = this;
+    }
+
+}
