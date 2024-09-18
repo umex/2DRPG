d1bdcab557689071489c6b95d793db9321c7f3ad
dobrez <dejan.obrez@gmail.com>
2024-09-18 21:56:27 +0200

    added a sword (throw) skill

---
+++ Assets/Scripts/Skills/Sword_Skill.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Sword_Skill : Skill
+{
+    [Header("Skill info")]
+    [SerializeField] private GameObject swordPrefab;
+    [SerializeField] private Vector2 launchForce;
+    [SerializeField] private float swordGravity;
+}
