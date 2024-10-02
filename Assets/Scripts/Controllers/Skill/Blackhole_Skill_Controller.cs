87e97a23c37937067510c147b3d6e53be57a838f
dobrez <dejan.obrez@gmail.com>
2024-10-02 20:37:11 +0200

    created a blackhole skill (sprite and class) with growing functionality

---
+++ Assets/Scripts/Controllers/Skill/Blackhole_Skill_Controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Blackhole_Skill_Controller : MonoBehaviour
+{
+    private float maxSize= 15;
+    private float growSpeed= 1;
+
+    private bool canGrow = true;
+
+    private void Update()
+    {
+
+
+        if (canGrow)
+        {
+            // lerp allows us to transition slowly towards the end
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+
+    }
+
+}
