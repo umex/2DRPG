4dc6bcf9c1c9547f5dea7a2ce8e26e9233d24393
dobrez <dejan.obrez@gmail.com>
2024-10-02 21:17:23 +0200

    hotkeys now appear over enemies affected by blackhole, pressing keys makes puts them on the list

--- Assets/Scripts/Controllers/Skill/Blackhole_HotKey_Controller.cs
+++ Assets/Scripts/Controllers/Skill/Blackhole_HotKey_Controller.cs
@@ -1,5 +1,37 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class Blackhole_HotKey_Controller : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    private KeyCode myHotKey;
+    //this is how we access to the textmeshpro component
+    private TextMeshProUGUI myText;
+
+    private Transform myEnemy;
+    private Blackhole_Skill_Controller blackHole;
+
+    public void SetupHotKey(KeyCode _myNewHotKey, Transform _myEnemy, Blackhole_Skill_Controller _myBlackHole)
+    {
+        sr = GetComponent<SpriteRenderer>();
+        myText = GetComponentInChildren<TextMeshProUGUI>();
+
+        myEnemy = _myEnemy;
+        blackHole = _myBlackHole;
+
+        myHotKey = _myNewHotKey;
+        myText.text = _myNewHotKey.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(myHotKey))
+        {
+
+            blackHole.AddEnemyToList(myEnemy);
+
+            myText.color = Color.clear;
+            sr.color = Color.clear;
+        }
+    }
 }
--- Assets/Scripts/Controllers/Skill/Blackhole_Skill_Controller.cs
+++ Assets/Scripts/Controllers/Skill/Blackhole_Skill_Controller.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Blackhole_Skill_Controller : MonoBehaviour
 {
-    private float maxSize= 15;
-    private float growSpeed= 1;
+    [SerializeField] private GameObject hotKeyPrefab;
+    [SerializeField] private List<KeyCode> keyCodeList;
 
-    private bool canGrow = true;
+    public float maxSize= 15;
+    public float growSpeed= 1;
+
+    public bool canGrow = true;
+
+    public List<Transform> targets = new List<Transform>();
 
     private void Update()
     {
@@ -24,6 +30,18 @@ public class Blackhole_Skill_Controller : MonoBehaviour
         if (collision.GetComponent<Enemy>() != null)
         {
             collision.GetComponent<Enemy>().FreezeTime(true);
+
+            //Quaternion.identity cause we dont want to rotate it
+            GameObject newHotKey = Instantiate(hotKeyPrefab, collision.transform.position + new Vector3(0, 2), Quaternion.identity);
+
+            KeyCode choosenKey = keyCodeList[Random.Range(0, keyCodeList.Count)];
+            keyCodeList.Remove(choosenKey); //so that we cannot chose it anymore
+
+            Blackhole_HotKey_Controller newHotKeyScript = newHotKey.GetComponent<Blackhole_HotKey_Controller>();
+
+            newHotKeyScript.SetupHotKey(choosenKey, collision.transform, this);
         }
     }
+
+    public void AddEnemyToList(Transform _enemyTransform) => targets.Add(_enemyTransform);
 }
