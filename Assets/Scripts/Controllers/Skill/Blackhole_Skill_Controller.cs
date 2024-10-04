8877d0edd506a2d117982f8b6c63c6b0d01acb24
dobrez <dejan.obrez@gmail.com>
2024-10-04 22:36:48 +0200

    added limit on attacks, destroyed hotkeys after all the attacks,

--- Assets/Scripts/Controllers/Skill/Blackhole_Skill_Controller.cs
+++ Assets/Scripts/Controllers/Skill/Blackhole_Skill_Controller.cs
@@ -8,15 +8,18 @@ public class Blackhole_Skill_Controller : MonoBehaviour
 
     public float maxSize = 15;
     public float growSpeed = 1;
+    private float shrinkSpeed = 1;
 
     public bool canGrow = true;
-    private bool canAttack;
+    private bool canShrink;
+    private bool cloneAttackReleased;
 
     private int amountOfAttacks = 3;
     private float cloneAttackCooldown = .3f;
     private float cloneAttackTimer;
 
     public List<Transform> targets = new List<Transform>();
+    private List<GameObject> createdHotKey = new List<GameObject>();
 
     private void Update()
     {
@@ -24,10 +27,20 @@ public class Blackhole_Skill_Controller : MonoBehaviour
 
         CloneAttackLogic();
 
-        if (canGrow)
+        if (canGrow && !canShrink)
         {
             // lerp allows us to transition slowly towards the end
             transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+
+        if (canShrink)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(-1, -1), shrinkSpeed * Time.deltaTime);
+
+            if (transform.localScale.x < 0)
+            {
+                Destroy(gameObject);
+            }
         }
 
     }
@@ -50,9 +63,9 @@ public class Blackhole_Skill_Controller : MonoBehaviour
             return;
         }
 
-
         //Quaternion.identity cause we dont want to rotate it
         GameObject newHotKey = Instantiate(hotKeyPrefab, collision.transform.position + new Vector3(0, 2), Quaternion.identity);
+        createdHotKey.Add(newHotKey);
 
         KeyCode choosenKey = keyCodeList[Random.Range(0, keyCodeList.Count)];
         keyCodeList.Remove(choosenKey); //so that we cannot chose it anymore
@@ -66,10 +79,10 @@ public class Blackhole_Skill_Controller : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            canAttack = true;
+            cloneAttackReleased = true;
         }
 
-        if (cloneAttackTimer < 0 && canAttack)
+        if (cloneAttackTimer < 0 && cloneAttackReleased && amountOfAttacks > 0)
         {
             cloneAttackTimer = cloneAttackCooldown;
 
@@ -78,11 +91,40 @@ public class Blackhole_Skill_Controller : MonoBehaviour
             float xOffset;
 
             if (Random.Range(0, 100) > 50)
+            {
                 xOffset = 2;
+            }
             else
+            {
                 xOffset = -2;
+            }
 
             SkillManager.instance.clone.CreateClone(targets[randomIndex], new Vector3(xOffset, 0));
+
+            amountOfAttacks--;
+
+            if (amountOfAttacks <= 0)
+            {
+                Invoke("FinishBlackHoleAbility", 1f);
+            }
+        }
+    }
+
+    private void FinishBlackHoleAbility()
+    {
+        DestroyHotKeys();
+        canShrink = true;
+        cloneAttackReleased = false;
+    }
+
+    private void DestroyHotKeys()
+    {
+        if (createdHotKey.Count <= 0)
+            return;
+
+        for (int i = 0; i < createdHotKey.Count; i++)
+        {
+            Destroy(createdHotKey[i]);
         }
     }
 
